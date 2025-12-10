932d18e6cf8bf7983eeb28e949d128914098997c
shenzhaolong <shenzhaolong@buaa.edu.cn>
2025-12-10 18:15:17 +0800

    README_zh.MD: 完善franka control说明

--- lerobot_robot_franka/lerobot_robot_franka/config_franka.py
+++ lerobot_robot_franka/lerobot_robot_franka/config_franka.py
@@ -11,6 +11,8 @@ class FrankaConfig(RobotConfig):
     gripper_reverse: bool = True
     robot_ip: str = "172.16.0.2"
     gripper_bin_threshold: float = 0.98
+    gripper_max_open: float = 0.0801  # gripper max open width in meters
     debug: bool = True
     close_threshold: float = 0.7
     cameras: dict[str, CameraConfig] = field(default_factory=dict)
+
